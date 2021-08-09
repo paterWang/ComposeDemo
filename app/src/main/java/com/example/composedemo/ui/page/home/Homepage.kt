2c664194a6ebbe32e3b055c76b5475b46fba93af
paterWang <1210532670@qq.com>
2021-08-09 13:13:48 +0800

    push代码

--- app/src/main/java/com/example/composedemo/ui/page/home/Homepage.kt
+++ app/src/main/java/com/example/composedemo/ui/page/home/Homepage.kt
@@ -41,6 +41,7 @@ fun HomePage(
         content = {
             Column {
                 searchBar()
+
                 val titles = mutableListOf(" 首页", "广场", "知识体系", "导航", "公众号", "项目", "问答")
                 val pagerState = rememberPagerState(pageCount = titles.size)
                 TopTapBar(
